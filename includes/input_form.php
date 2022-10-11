07fe6bda9139a2f8822d55416170128490e5c44d
STRUAN HENDERSON <Struanjh@gmail.com>
2022-10-11 12:21:36 +0100

    Added CSS Styling for Input Page

--- includes/input_form.php
+++ includes/input_form.php
@@ -13,46 +13,65 @@
     ?>
 
     <form method="post">
-        <label for="restaurant_name">Restaurant Name</label>
-        <input type="text" name="restaurant_name" id="restaurant_name" value="<?php if($_SERVER["REQUEST_METHOD"] == "POST" && !$validationPassed) echo $submittedData['name']; ?>">
-        <span class="error"><?php if($_SERVER["REQUEST_METHOD"] == "POST") echo $formValidation['nameErr']; ?></span>
-        <fieldset>
+        <div class="form-section">
+            <div class="form-sub-section">
+                <label for="restaurant_name">Restaurant Name</label>
+                <input type="text" name="restaurant_name" id="restaurant_name" value="<?php if($_SERVER["REQUEST_METHOD"] == "POST" && !$validationPassed) echo $submittedData['name']; ?>">
+                <span class="error"><?php if($_SERVER["REQUEST_METHOD"] == "POST") echo $formValidation['nameErr']; ?></span>
+            </div>
+            <div class="form-sub-section">
+                <label for="cuisine">Cuisine</label>
+                <select name="cuisine" id="cuisine">
+                    <?php
+                        $cuisineArr = ["korean", "japanese", "thai", "vietnamese", "american"];
+                        for ($i=0; $i<count($cuisineArr); $i++) {
+                            if(isset($_POST['submit']) && !$validationPassed && $submittedData['cuisine'] == $cuisineArr[$i]) {
+                                $selected = 'selected';
+                            } else {
+                                $selected = '';
+                            }
+                    ?>
+                        <option value=<?=$cuisineArr[$i]?> <?=$selected?>> <?=$cuisineArr[$i]?> </option>
+                    <?php 
+                        } 
+                    ?> 
+                </select>
+                <span class="error"><?php if($_SERVER["REQUEST_METHOD"] == "POST") echo $formValidation['cuisineErr']; ?></span>
+            </div>
+            <fieldset class="form-sub-section">
+                <legend>Price</legend>
+                <div>
+                    <label for="low_price">Budget</label>
+                    <input type="radio" name="price" id="low_price" value="low_price">
+                </div>
+                <div>
+                    <label for="medium_price">Normal</label>
+                    <input type="radio" name="price" id="medium_price" value="medium_price">
+                </div>
+                <div>
+                    <label for="high_price">Expensive</label>
+                    <input type="radio" name="price" id="high_price" value="high_price">
+                </div>
+                <span class="error"><?php if($_SERVER["REQUEST_METHOD"] == "POST") echo $formValidation['priceErr']; ?></span>
+            </fieldset>
+        </div>
+        <fieldset class="form-section">
             <legend>Location Info</legend>
-            <label for="city">City</label>
-            <input type="text" id="city" name="city" value="<?php if($_SERVER["REQUEST_METHOD"] == "POST" && !$validationPassed) echo $submittedData['city']; ?>">
-            <span class="error"><?php if($_SERVER["REQUEST_METHOD"] == "POST") echo $formValidation['cityErr']; ?></span>
-            <label for="district">District</label>
-            <input type="text" id="district" name="district" value="<?php if($_SERVER["REQUEST_METHOD"] == "POST" && !$validationPassed) echo $submittedData['district']; ?>">
-            <span class="error"><?php if($_SERVER["REQUEST_METHOD"] == "POST") echo $formValidation['districtErr']; ?></span>
-            <label for="postcode">Postcode</label>
-            <input type="text" id="postcode" name="postcode" <?php if($_SERVER["REQUEST_METHOD"] == "POST"&& !$validationPassed) echo $submittedData['postcode']; ?>>
-            <span class="error"><?php if($_SERVER["REQUEST_METHOD"] == "POST") echo $formValidation['postcodeErr']; ?></span>
-        </fieldset>
-        <label for="cuisine">Cuisine</label>
-        <select name="cuisine" id="cuisine">
-            <?php
-                $cuisineArr = ["korean", "japanese", "thai", "vietnamese", "american"];
-                for ($i=0; $i<count($cuisineArr); $i++) {
-                    if(isset($_POST['submit']) && !$validationPassed && $submittedData['cuisine'] == $cuisineArr[$i]) {
-                        $selected = 'selected';
-                    } else {
-                        $selected = '';
-                    }
-            ?>
-                <option value=<?=$cuisineArr[$i]?> <?=$selected?>> <?=$cuisineArr[$i]?> </option>
-            <?php 
-                } 
-            ?> 
-        </select>
-        <span class="error"><?php if($_SERVER["REQUEST_METHOD"] == "POST") echo $formValidation['cuisineErr']; ?></span>
-        <fieldset>
-            <label for="low_price">Budget</label>
-            <input type="radio" name="price" id="low_price" value="low_price">
-            <label for="medium_price">Normal</label>
-            <input type="radio" name="price" id="medium_price" value="medium_price">
-            <label for="high_price">Expensive</label>
-            <input type="radio" name="price" id="high_price" value="high_price">
-            <span class="error"><?php if($_SERVER["REQUEST_METHOD"] == "POST") echo $formValidation['priceErr']; ?></span>
+            <div class="form-sub-section">
+                <label for="city">City</label>
+                <input type="text" id="city" name="city" value="<?php if($_SERVER["REQUEST_METHOD"] == "POST" && !$validationPassed) echo $submittedData['city']; ?>">
+                <span class="error"><?php if($_SERVER["REQUEST_METHOD"] == "POST") echo $formValidation['cityErr']; ?></span>
+            </div>
+            <div class="form-sub-section">
+                <label for="district">District</label>
+                <input type="text" id="district" name="district" value="<?php if($_SERVER["REQUEST_METHOD"] == "POST" && !$validationPassed) echo $submittedData['district']; ?>">
+                <span class="error"><?php if($_SERVER["REQUEST_METHOD"] == "POST") echo $formValidation['districtErr']; ?></span>
+            </div>
+            <div class="form-sub-section">
+                <label for="postcode">Postcode</label>
+                <input type="text" id="postcode" name="postcode" <?php if($_SERVER["REQUEST_METHOD"] == "POST"&& !$validationPassed) echo $submittedData['postcode']; ?>>
+                <span class="error"><?php if($_SERVER["REQUEST_METHOD"] == "POST") echo $formValidation['postcodeErr']; ?></span>
+            </div>
         </fieldset>
         <button type="submit" name="submit" value="submit">Submit</button>
     </form>
